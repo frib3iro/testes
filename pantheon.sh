8ee7d4b56e20dd0f8ee08ed2e7f7e0b9ab864e2d
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2020-11-30 19:44:34 -0300

    Add files via upload

---
+++ pantheon.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#----------------------------------------------------------------------
+# Script    : phanteon.sh
+# Descrição :
+# Versão    : 0.1
+# Autor     : Fabio Junior Ribeiro <rib3iro@live.com>
+# Data      : 30/11/2020
+# Licença   : GNU/GPL v3.0
+#----------------------------------------------------------------------
+# Uso       :
+#----------------------------------------------------------------------
+
+yay -S pantheon-session-git pantheon-default-settings-git gnome-settings-daemon-elementary gnome-settings-daemon-ubuntu pantheon-print-git elementary-icon-theme lightdm-pantheon-greeter ttf-droid ttf-opensans ttf-raleway ttf-dejavu gnu-free-fonts ttf-liberation pantheon-files pantheon-terminal pantheon-code pantheon-calculator pantheon-music pantheon-videos pantheon-calendar-git pantheon-mail-git pantheon-screenshot eidete-bzr pantheon-photos pantheon-camera-git simple-scan switchboard pantheon-screencast elementary-wallpapers gtk-theme-elementary sound-theme-elementary
+
+
+
+
